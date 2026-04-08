7560291be0dab8a31603b6e5cd7d31fa74977162
Serhii <spridoloba@gmail.com>
2026-04-08 16:55:01 +0300

    feat: solution for task

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -11,15 +11,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "testresource" {
+  name     = "test-resources"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "test" {
+  name                     = "teststorageacc"
+  resource_group_name      = azurerm_resource_group.testresource.name
+  location                 = azurerm_resource_group.testresource.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
